dbe3e4b9a03327a5676063c69f545726182ec970
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-07-26 22:37:41 -0300

    Teste da Geração do pedido.

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,20 @@
+﻿using Store.Domain.Entities;
+using Xunit;
+
+namespace Store.Tests.Entities
+{
+    public class OrderTests
+    {
+        [Fact]
+        public void Dado_Um_Novo_Pedido_Valido_Ele_Deve_Gerar_Um_Novo_Numero_Com_8_Caracteres()
+        {
+            var customer = new Customer("Teste", "teste@teste.com.br");
+
+            var order = new Order(customer, 15.00m, null);
+
+            Assert.Equal(8, order.Number.Length);
+
+        }
+
+    }
+}
