c5e6b37909589d280558180aefb430c451495694
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-03 22:18:20 -0300

    Adiciona teste para validar status de após pedido cancelado.

--- Store.Tests/Entities/OrderTests.cs
+++ Store.Tests/Entities/OrderTests.cs
@@ -14,28 +14,38 @@ namespace Store.Tests.Entities
         [Fact]
         public void Dado_Um_Novo_Pedido_Valido_Ele_Deve_Gerar_Um_Novo_Numero_Com_8_Caracteres()
         {
-            var order = new Order(_customer, 0m, null);
+            var order = new Order(_customer, 0, null);
 
-            Assert.Equal(8, order.Number.Length);
+            Assert.Equal(order.Number.Length, 8);
         }
 
         [Fact]
         public void Dado_Um_Novo_Pedido_Seu_Status_Deve_Ser_AguardandoPagamento()
         {
-            var order = new Order(_customer, 0m, null);
+            var order = new Order(_customer, 0, null);
 
-            Assert.Equal(EOrderStatus.WaitingPayment, order.Status);
+            Assert.Equal(order.Status, EOrderStatus.WaitingPayment);
         }
 
         [Fact]
         public void Dado_Um_Pagamento_Do_Pedido_Seu_Status_Deve_Ser_Aguardando_Entrega()
         {
-            var order = new Order(_customer, 0m, null);
+            var order = new Order(_customer, 0, null);
             order.AddItem(_product, 1);
             order.Pay(10);
 
-            Assert.Equal(EOrderStatus.WaitingDelivery, order.Status);
+            Assert.Equal(order.Status, EOrderStatus.WaitingDelivery);
         }
+
+        [Fact]
+        public void Dado_Um_Pedido_Cancelado_Seu_Status_Deve_Ser_Cancelado()
+        {
+            var order = new Order(_customer, 0, null);
+            order.Cancel();
+
+            Assert.Equal(order.Status, EOrderStatus.Canceled);
+        }
+
 
     }
 }
