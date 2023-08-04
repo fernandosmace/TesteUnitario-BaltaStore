8fdad0f020df8f2ef2aa003a8a21467633d5dfa1
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-03 23:31:08 -0300

    Criados repositorios da aplicação com interfaces

---
+++ Store.Domain/Repositories/ICustomerRepository.cs
@@ -0,0 +1,7 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories.Interfaces;
+public interface ICustomerRepository
+{
+    Customer Get(string document);
+}
---
+++ Store.Domain/Repositories/IProductRepository.cs
@@ -0,0 +1,7 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Domain.Repositories.Interfaces;
+public interface IProductRepository
+{
+    IEnumerable<Product> Get(IEnumerable<Guid> ids);
+}
