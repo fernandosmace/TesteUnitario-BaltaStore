b75d8d7ce22b151164862519e54b48e0fa427ca8
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-11 21:38:42 -0300

    Criados testes para queries dee Product.

---
+++ Store.Tests/Queries/ProductQueriesTests.cs
@@ -0,0 +1,39 @@
+﻿using Store.Domain.Entities;
+using Store.Domain.Queries;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Store.Tests.Queries
+{
+    public class ProductQueriesTests
+    {
+        private IList<Product> _products;
+
+        public ProductQueriesTests()
+        {
+            _products = new List<Product>();
+            _products.Add(new Product("Produto 01", 10, true));
+            _products.Add(new Product("Produto 02", 20, true));
+            _products.Add(new Product("Produto 03", 30, true));
+            _products.Add(new Product("Produto 04", 40, false));
+            _products.Add(new Product("Produto 05", 50, false));
+        }
+
+        [Fact]
+        [Trait("Category", "Queries")]
+        public void Dado_A_Consulta_De_Produtos_Ativos_Deve_Retornar_3()
+        {
+            var result = _products.AsQueryable().Where(ProductQueries.GetActiveProducts());
+            Assert.Equal(result.Count(), 3);
+        }
+
+        [Fact]
+        [Trait("Category", "Queries")]
+        public void Dado_A_Consulta_De_Produtos_Inativos_Deve_Retornar_2()
+        {
+            var result = _products.AsQueryable().Where(ProductQueries.GetInactiveProducts());
+            Assert.Equal(result.Count(), 2);
+        }
+    }
+}
