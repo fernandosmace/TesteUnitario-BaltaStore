b92128b73292a96851b5f527ed855dfe22a2579e
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2023-08-11 22:37:11 -0300

    Criação dos testeste para os commands.

---
+++ Store.Tests/Commands/CreateOrderCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Store.Domain.Commands;
+using System;
+using Xunit;
+
+namespace Store.Tests.Commands
+{
+    public class CreateOrderCommandTests
+    {
+        [Fact]
+        [Trait("Category", "Handlers")]
+        public void Dado_Um_Comand_Invalido_O_Pedido_Deve_Nao_Deve_Ser_Gerado()
+        {
+            var command = new CreateOrderCommand();
+            command.Customer = "";
+            command.ZipCode = "13411080";
+            command.PromoCode = "12345678";
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Items.Add(new CreateOrderItemCommand(Guid.NewGuid(), 1));
+            command.Validate();
+
+            Assert.Equal(command.Valid, false);
+        }
+    }
+}
